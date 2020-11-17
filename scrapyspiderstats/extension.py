5ffe573369121713b11efa3e94dcc3060c472aa8
Sitoi <133397418@qq.com>
2020-11-17 08:56:27 +0800

    优化代码

--- scrapyspiderstats/extension.py
+++ scrapyspiderstats/extension.py
@@ -30,17 +30,14 @@ class SpiderStats(LogStats):
         interval = crawler.settings.getfloat("LOGSTATS_INTERVAL")
         if not interval:
             raise NotConfigured
-
         o = cls(client, stats_mongodb_db, stats_mongodb_col, crawler.stats, interval)
-        # o = cls(crawler.stats, interval)
         crawler.signals.connect(o.spider_opened, signal=signals.spider_opened)
         crawler.signals.connect(o.spider_closed, signal=signals.spider_closed)
         return o
 
     def spider_opened(self, spider):
-
         stats_id = uuid.uuid4().hex
-        self.stats.set_value("StatsId", stats_id, spider=spider)
+        self.stats.set_value("stats_id", stats_id, spider=spider)
         self.stats.set_value("spider_name", spider.name, spider=spider)
         logger.info("Spider Stats ID: {0}".format(stats_id))
         self.task = task.LoopingCall(self.log, spider)
@@ -60,24 +57,10 @@ class SpiderStats(LogStats):
         insert_item = {}
         for key, value in self.stats.get_stats().items():
             insert_item[str(key).replace(".", "-")] = value
-        self.col.insert(insert_item, check_keys=False)
+        self.col.insert_one(insert_item)
         logger.info(self.stats.get_stats())
 
     def spider_closed(self, spider, reason):
-        items = self.stats.get_value("item_scraped_count", 0)
-        pages = self.stats.get_value("response_received_count", 0)
-        irate = (items - self.itemsprev) * self.multiplier
-        prate = (pages - self.pagesprev) * self.multiplier
-        self.stats.set_value("pages", pages)
-        self.stats.set_value("pagerate", prate)
-        self.stats.set_value("items", items)
-        self.stats.set_value("itemrate", irate)
-        self.stats.set_value("record_time", datetime.utcnow())
-        insert_item = {}
-        for key, value in self.stats.get_stats().items():
-            insert_item[str(key).replace(".", "-")] = value
-        self.col.insert(self.stats.get_stats(), check_keys=False)
-
-        logger.info(self.stats.get_stats())
+        self.log(spider)
         if self.task and self.task.running:
             self.task.stop()
